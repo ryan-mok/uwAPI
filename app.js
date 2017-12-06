94fda3398bb334e7d3309c5198454787139a8aca
Ryan Mok <ryan.mok@edu.uwaterloo.ca>
2017-12-06 15:15:04 -0500

    Fixed syntax error and added comments

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 'use strict';
 
+// Imports for Dialogflow
 const App = require('actions-on-google').DialogflowApp;
 
-const http = require('http');
+// Imports for node rest client
 const Client = require('node-rest-client').Client;
 const client = new Client();
 
-const KEY = process.env.KEY;
-
+// Imports for express
 const express = require('express')
 const bodyParser = require('body-parser')
 const expressApp = express()
 expressApp.use(bodyParser.json())
 expressApp.set('port', (process.env.PORT || 5000))
 
+// Get key from environment
+const KEY = process.env.KEY;
+
+// List of functions and arguments
+const CLASS_NAME = 'class_name';
+const SUBJECT_ARGUMENT = 'subject';
+const NUMBER_ARGUMENT = 'number';
+
+const CLASS_DESCRIPTION = 'class_description';
+const SUBJECT_ARGUMENT = 'subject';
+const NUMBER_ARGUMENT = 'number';
+
+// Express function
 expressApp.post('/webhook', function (request, response) {
 	const app = new App({request, response});
 
 	function className (app) {
-		const CLASS_NAME = 'class_name';
-		const SUBJECT_ARGUMENT = 'subject';
-		const NUMBER_ARGUMENT = 'number';
-
 		let subject = app.getArgument(SUBJECT_ARGUMENT);
 		let number = app.getArgument(NUMBER_ARGUMENT);
 
@@ -42,10 +51,6 @@ expressApp.post('/webhook', function (request, response) {
 	}
 
 	function classDescription (app) {
-		const CLASS_DESCRIPTION = 'class_description';
-		const SUBJECT_ARGUMENT = 'subject';
-		const NUMBER_ARGUMENT = 'number';
-
 		let subject = app.getArgument(SUBJECT_ARGUMENT);
 		let number = app.getArgument(NUMBER_ARGUMENT);
 
@@ -69,8 +74,4 @@ expressApp.post('/webhook', function (request, response) {
 	actionMap.set(CLASS_NAME, className);
 	actionMap.set(CLASS_DESCRIPTION, classDescription);
 	app.handleRequest(actionMap);
-})
-
-expressApp.listen(expressApp.get('port'), function () {
-	console.log('Webhook started')
 })
