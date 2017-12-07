9e5400b68e623a14285d87cb4ade30059c62ddb1
Ryan Mok <ryanm@RyanM.local>
2017-12-06 20:27:22 -0500

    Changed response output to add question
    Added eslint

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const Client = require('node-rest-client').Client;
 const client = new Client();
 
 // Imports for express
-const express = require('express')
-const bodyParser = require('body-parser')
-const expressApp = express()
-expressApp.use(bodyParser.json())
-expressApp.set('port', (process.env.PORT || 5000))
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressApp = express();
+expressApp.use(bodyParser.json());
+expressApp.set('port', (process.env.PORT || 5000));
 
 // Get key from environment
 const KEY = process.env.KEY;
@@ -44,7 +44,7 @@ expressApp.post('/webhook', function (request, response) {
 		};
 		client.get(url, args, (data, postResponse) => {
 			if (postResponse.statusCode == 200) {
-				app.ask(subject + ' ' + number + ' is ' + data.data.title + '.');
+				app.ask(subject + ' ' + number + ' is ' + data.data.title + '. What else would you like to know?');
 			}
 			else {
 				app.tell('Oops. There was an error.');
@@ -63,7 +63,7 @@ expressApp.post('/webhook', function (request, response) {
 		};
 		client.get(url, args, (data, postResponse) => {
 			if (postResponse.statusCode == 200) {
-				app.ask('Here\'s a brief description I found about ' + subject + ' ' + number + ': ' + data.data.description.split('[')[0]);
+				app.ask('Here\'s a brief description I found about ' + subject + ' ' + number + ': ' + data.data.description.split('[')[0] + ' What else would you like to know?');
 			}
 			else {
 				app.tell('Oops. There was an error.');
@@ -85,10 +85,10 @@ expressApp.post('/webhook', function (request, response) {
 				if (data.data && data.data[0] && data.data[0].classes && data.data[0].classes[0] && data.data[0].classes[0].instructors && data.data[0].classes[0].instructors[0]) {
 					const firstName = data.data[0].classes[0].instructors[0].split(',')[1];
 					const lastName = data.data[0].classes[0].instructors[0].split(',')[0];
-					app.ask('The instructor for ' + subject + ' ' + number + ' is ' + firstName + ' ' + lastName + '.');
+					app.ask('The instructor for ' + subject + ' ' + number + ' is ' + firstName + ' ' + lastName + '. What else would you like to know?');
 				}
 				else {
-					app.ask('Sorry, it seems like ' + subject + ' ' + number + ' isn\'t offered right now.');
+					app.ask('Sorry, it seems like ' + subject + ' ' + number + ' isn\'t offered right now. What else would you like to know?');
 				}
 			}
 			else {
@@ -107,7 +107,7 @@ expressApp.post('/webhook', function (request, response) {
 		};
 		client.get(url, args, (data, postResponse) => {
 			if (postResponse.statusCode == 200) {
-				app.ask(building + ' stands for ' + data.data.building_name + '.');
+				app.ask(building + ' stands for ' + data.data.building_name + '. What else would you like to know?');
 			}
 			else {
 				app.tell('Oops. There was an error.');
@@ -121,8 +121,8 @@ expressApp.post('/webhook', function (request, response) {
 	actionMap.set(CLASS_PROF, classProf);
 	actionMap.set(BUILDING_NAME, buildingName);
 	app.handleRequest(actionMap);
-})
+});
 
 expressApp.listen(expressApp.get('port'), function () {
-	console.log('Webhook started')
-}) 
+	console.log('Webhook started');
+});
